0f269c06f53d1e371e773bc46d31bedb5d2b4347
kbanico <code4uguam@gmail.com>
2017-03-17 14:36:20 +0000

    add game model and games controller with new create and show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'games/new'
+
+  get 'games/create'
+
+  get 'games/show'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
