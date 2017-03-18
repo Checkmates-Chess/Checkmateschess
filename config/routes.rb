953e0159fa36083661e2bcc27efac66c33d6a20f
kbanico <code4uguam@gmail.com>
2017-03-18 06:07:53 +0000

    add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  # get 'games/new'
-
-  # get 'games/create'
-
-  # get 'games/show'
-
-  resources :games
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
+
+  # games
+  resources :games
+
+  # user profile
+  resources :users, only: :show
 end
