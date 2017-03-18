69f87b27bc9691c25aff0fee634fff119d85b09b
kbanico <code4uguam@gmail.com>
2017-03-18 05:17:06 +0000

    fix navbar add validation to user.rb and game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3,
+    maximum: 30 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, length: { minimum: 3,
+    maximum: 16 }
+
+  #validates_uniqueness_of :username
 end
