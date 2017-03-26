12d06e2f7d78fd87bb8a2491763f38e7047165ee
Daniel <ddegrassi17@gmail.com>
2017-03-26 14:52:15 +0000

    added basic tests: successfully show homepage, lobby page and user show page(as well as created a factory for users)

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,1 +1,12 @@
 # Factories go here
+
+FactoryGirl.define do
+	factory :user do
+		sequence :email do |n|
+			'dummyEmail#{n}@gmail.com'
+		end
+		username 'hippoman'
+		password 'secretPassword'
+		password_confirmation 'secretPassword'
+	end
+end
