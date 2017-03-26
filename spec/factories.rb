b6638a1c3387f8749e98622fd08e33ff101cc0a9
Evan Cording <evancording@gmail.com>
2017-03-26 18:57:39 +0000

    added factories for game and piece

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,4 +9,25 @@ FactoryGirl.define do
 		password 'secretPassword'
 		password_confirmation 'secretPassword'
 	end
+
+  factory :game do
+    game_title 'meat salad forever'
+    player1_id 123
+    player2_id 456
+    player_turn "black"
+    winner_id 0
+    game_id 123456
+  end
+
+  factory :piece do
+    player_id 123
+    game_id 123456
+    piece_type "bishop"
+    piece_color "black"
+    piece_status "pretty chill"
+    x_coordinate 5
+    y_coordinate 5
+    piece_id 1
+  end
+
 end
