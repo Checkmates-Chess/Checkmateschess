2c755fcbd86867fbd26cd8e7a2dd0d6412e813e5
Evan Cording <evancording@gmail.com>
2017-03-27 18:30:41 +0000

    initial sti definition in piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,10 @@
 class Piece < ApplicationRecord
-  belongs_to: game
-  belongs_to: user
+  belongs_to :game
+  belongs_to :user
+
+  self.inheritance_column = :piece_type
+
+  def self.piece_types
+    ["Pawn", "Knight", "Bishop", "Rook", "Queen", "King"]
+  end
 end
