f0a238b370d98802af7f2dcd4e2cfeb10e5763aa
Evan Cording <evancording@gmail.com>
2017-03-28 02:59:58 +0000

    added models for different piece types

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,4 +7,12 @@ class Piece < ApplicationRecord
   def self.piece_types
     ["Pawn", "Knight", "Bishop", "Rook", "Queen", "King"]
   end
+
+  scope :pawns, -> { where(piece_type: "Pawn") }
+  scope :knights, -> { where(piece_type: "Knight") }
+  scope :bishops, -> { where(piece_type: "Bishop") }
+  scope :rooks, -> { where(piece_type: "Rook") }
+  scope :queens, -> { where(piece_type: "Queen") }
+  scope :kings, -> { where(piece_type: "King") }
 end
+
