f77a8165ed740acedb7aff8ffe447bf779a14e2f
Evan Cording <evancording@gmail.com>
2017-03-28 18:06:13 +0000

    updated show view with join link to game#update, updated games controller update action and permitted fields under game_params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,11 +13,13 @@ class GamesController < ApplicationController
   end
 
   def update
+    current_game.update_attributes(:player_black_id => current_user.id)
+    redirect_to game_path(current_game)
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:name)
+    params.require(:game).permit(:game_title, :player_black_id, :player_white_id, :player_turn, :winner_id)
   end
 end
