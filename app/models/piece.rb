ee25855b935a58fa2ebb034344239af6ffa1e136
Daniel <ddegrassi17@gmail.com>
2017-04-01 10:28:26 +0000

    same as last commit, forgot to save something to it

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -25,66 +25,48 @@ class Piece < ApplicationRecord
 #   [R,N,B,K,Q,B,N,R],
 #   [P,P,P,P,P,P,P,P],
 #   [o,o,o,o,o,o,o,o],
-#   [o,o,o,3,o,o,o,o],
-#   [o,o,o,o,o,o,o,o],
+#   [o,o,o,"start",o,o,o,3],
+#   [o,o,o,o,o,o,o,"end"],
 #   [o,o,o,o,o,o,o,o],
 #   [WP,WP,WP,WP,WP,WP,WP,WP],
 #   [WR,WN,WB,WK,WQ,WB,WN,WR]
 #   ]
-#   start_vertical = 3 
-#   start_horizontal= 3
-#   end_vertical = 5
-#   end_horizontal = 4
-#   puts board[start_vertical][start_horizontal]
-#   puts board[end_vertical][end_horizontal]
- 
+
   # checks for obstruction using a board state, start and
   # end locations, and any open spaces being designated
-  # "open space"
+  # "open space".  
+  # possibly need to edit variables depending on end state of inputs
+
   def is_obstructed?(board, start_vertical,start_horizontal,end_vertical,end_horizontal)
     # raises error if end position is same as starting
     raise 'Invalid Input, destination must be different from start' if start_vertical == end_vertical && start_horizontal == end_horizontal
-
-    # need to raise an error if invalid move
-    
-    # up/right
-    if start_vertical > end_vertical && start_horizontal < end_horizontal
-      return move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, -1, 1)
-    # up/left
-    elsif start_vertical > end_vertical && start_horizontal > end_horizontal
-      return move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, -1, -1)
-    # down/left
-    elsif start_vertical < end_vertical && start_horizontal > end_horizontal
-      return move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, 1, -1)
-    # down/right
-    elsif start_vertical < end_vertical && start_horizontal < end_horizontal
-      return move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, 1, 1)
-    # up/down 
-
-    # change to elsif start_horizontal === end_horizontal && start_vertical < end_vertical
-    # for uniformity
-    elsif start_horizontal === end_horizontal
-      if start_vertical < end_vertical
-        return move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, 1, 0)
-      else
-        return move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, -1, 0)
-      end
-    # left/right
-    elsif start_vertical === end_vertical
-      if start_horizontal < end_horizontal
-        move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, 0, 1)
-      else
-      move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, 0, -1)
-      end
+    # raises error if invalid move, otherwise runs
+    if start_vertical == end_vertical || start_horizontal == end_horizontal    
+       move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal)
+    elsif ((start_vertical-end_vertical).abs != (start_horizontal - end_horizontal).abs)
+          raise "Invalid Input, not a diagonal horizontal or vertical move"
+    else  move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal)
     end
   end
   
+  # figures out which direction on the board to iterate
+  def get_incr(start_p, end_p)
+      incr = end_p - start_p
+      if incr != 0
+      incr = incr / incr.abs
+      end
+      return incr
+  end
+  
   # iterates by one square in whichever direction we specify and checks for "open space"
-  # 
-  def move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal, vert_incr, hor_incr)
+  def move_by_one(board, start_vertical, start_horizontal, end_vertical, end_horizontal)
     check_vertical = start_vertical
     check_horizontal = start_horizontal
-    while(check_vertical != end_vertical || check_horizontal != end_horizontal)
+    vert_incr = get_incr(start_vertical, end_vertical)
+
+    hor_incr= get_incr(start_horizontal, end_horizontal)
+    while(check_vertical != end_vertical || 
+      check_horizontal != end_horizontal)
       check_vertical += vert_incr
       check_horizontal += hor_incr
       if [[check_horizontal],[check_vertical]] === [[end_horizontal],[end_vertical]]
@@ -95,6 +77,5 @@ class Piece < ApplicationRecord
       end
     end
   end
-
- puts is_obstructed?(board, start_vertical ,start_horizontal, end_vertical, end_horizontal)
 end
+
