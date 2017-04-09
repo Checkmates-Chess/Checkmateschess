8a8345f46ff064584649f3d42739611d4c8601fd
kbanico <code4uguam@gmail.com>
2017-04-09 04:28:28 +0000

    cleaned up knight.rb

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,10 +3,7 @@ class Knight < Piece
   private
 
   def valid_move?(x, y)
-     # x, y
-     # x_coordinate, y_corrdinate
-     #
-     # (2,1) up right, (2,-1) up left, (1,2), [-2,-1,1,2]
+     # check valid move from piece.rb and check if x and y of knight is valid
      diff_x = (x - x_coordinate).abs
      diff_y = (y - y_coordinate).abs
      super(x, y) && (diff_x == 2 && diff_y == 1) || (diff_x == 1 && diff_y == 2)
