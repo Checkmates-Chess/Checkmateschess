919a3905369b82879ba5c0e3ff6bf97a711307f8
Evan Cording <evancording@gmail.com>
2017-04-13 10:53:22 +0000

    updated tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe GamesController, type: :controller do
   describe "games#update" do
     it "should assign current user id to player_black_id and host to player_white_id (for now) for open games" do
       game_to_join = FactoryGirl.create(:game)
-      joining_user = FactoryGirl.create(:user, email: "derp@gmail.com", username: "derpman")
+      joining_user = FactoryGirl.create(:user)
       sign_in joining_user
       patch :update, params: { id: game_to_join.id }
       game_to_join.reload
