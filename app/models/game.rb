4b1ac79701f53d8df515ca2a9a2b3b38b605fe8c
Michael Santo Domingo <msd2802@gmail.com>
2017-04-13 03:39:32 +0000

    Seems to be passing all tests now - still need to test a few things before merge

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,8 @@ class Game < ApplicationRecord
   has_many :pieces
   belongs_to :user
 
-  validates :game_title, presence: true, length: { minimum: 3, 
+  validates :game_title, presence: true, length: { minimum: 3,
     maximum: 30 }
+
+  scope :available, -> { where(player_black_id: nil).or(where(player_white_id: nil)) }
 end
