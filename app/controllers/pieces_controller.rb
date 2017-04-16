6ab9e5df3bd7907f0aecefcd3f7ba71cb010df57
Evan Cording <evancording@gmail.com>
2017-04-16 06:57:38 +0000

    remove pieces show action

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,10 +1,6 @@
 class PiecesController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @piece = Piece.find(params[:id])
-  end
-
   def update
     @piece = Piece.find(params[:id])
     @piece.update_attributes(piece_params)
