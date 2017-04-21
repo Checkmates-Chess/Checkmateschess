bc056758daf6da28b30d8451f9825e7643c62315
Willie Broder <willie.broder@gmail.com>
2017-04-21 02:04:14 +0000

    refactored piece and bishop tests

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -125,145 +125,81 @@ RSpec.describe PiecesController, type: :controller do
 	end
 
 	describe "valid_move? for Pieces model" do
+		game = FactoryGirl.create(:game)
 		o = nil
-		e = "end points"
-		x = "piece"
+	  e = "end points"
+	  x = "piece"
+		game.board = [
+							[@b_rook1,o,o,o,o,@b_bishop2,o,o],
+							[o,o,o,o,o,o,o,o],
+							[o,o,o,e,e,o,o,o],
+							[o,o,o,o,o,o,o,o],
+							[o,o,o,o,o,o,o,o],
+							[o,o,o,e,e,o,o,o],
+							[o,o,o,o,o,o,o,o],
+							[o,o,@w_bishop1,o,o,o,o,@w_rook2]
+						]
+    w_bishop1 = game.pieces.find_by_piece_name("w_bishop1")
+    b_bishop2 = game.pieces.find_by_piece_name("b_bishop2")
+    b_rook1 = game.pieces.find_by_piece_name("b_rook1")
+    w_rook2 = game.pieces.find_by_piece_name("w_rook2")
 
 		it "should prevent piece from moving off the board" do
-			game = FactoryGirl.create(:game)
-			game.board = [
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,e,e,o,o,o],
-								[o,o,o,o,x,o,o,o],
-								[o,o,x,o,o,x,o,o],
-							]
-
-			user = FactoryGirl.create(:user)
-	    piece = FactoryGirl.create(:bishop, piece_color: "white", x_coordinate: 2, y_coordinate: 7, user_id: user.id, game_id: game.id)
-
-	    expect(piece.valid_move?(1, 8)).to eq(false)
+	    expect(w_bishop1.valid_move?(1, 8)).to eq(false)
 		end
 
 		it "should allow piece to move when valid" do
-			game = FactoryGirl.create(:game)
-			game.board = [
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,e,e,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,@w_bishop1,o,o,@w_bishop2,o,o],
-							]
-			user = FactoryGirl.create(:user)
-	    piece1 = game.pieces.find_by_piece_name("w_bishop1")
-	    piece2 = game.pieces.find_by_piece_name("w_bishop2")
-
-	    expect(piece1.valid_move?(5, 4)).to eq(true)
-	    expect(piece2.valid_move?(5,3)).to eq(true)
+	    expect(w_bishop1.valid_move?(5, 4)).to eq(true)
+	    expect(b_bishop2.valid_move?(2, 3)).to eq(true)
+	    expect(b_rook1.valid_move?(0, 5)).to eq(true)
+	    expect(w_rook2.valid_move?(4, 7)).to eq(true)
 		end
 
 		it "should prevent piece to move when obstructed" do
 			game = FactoryGirl.create(:game)
-			game.board = [
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,e,e,o,o,o],
-								[o,o,o,o,x,o,o,o],
-								[o,o,x,o,o,@w_bishop2,o,o],
-							]
-			user = FactoryGirl.create(:user)
-	    piece = game.pieces.find_by_piece_name("w_bishop2")
+	    piece = game.pieces.find_by_piece_name("w_bishop1")
 
-	    expect(piece.valid_move?(5, 3)).to eq(false)
+	    expect(piece.valid_move?(5, 4)).to eq(false)
 		end
 	end
 
 	describe "valid_move? for Bishop" do
-
-		o = nil
-		e = "end points"
-		x = "piece"
-		# unobstructed board
-
-	  it "should allow bishops to move diagonally" do
 			game = FactoryGirl.create(:game)
+			o = nil
+		  e = "end points"
+		  x = "piece"
 			game.board = [
+								[o,o,@b_bishop1,o,o,@b_bishop2,o,o],
 								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
-								[o,o,o,o,o,o,o,o],
+								[o,o,o,e,e,o,o,o],
 								[o,o,o,o,o,o,o,o],
 								[o,o,o,o,o,o,o,o],
 								[o,o,o,e,e,o,o,o],
-								[o,o,o,o,x,o,o,o],
-								[o,o,@w_bishop1,o,o,x,o,o],
+								[o,o,o,o,o,o,o,o],
+								[o,o,@w_bishop1,o,o,@w_bishop2,o,o]
 							]
-			user = FactoryGirl.create(:user)
-	    bishop = game.pieces.find_by_piece_name("w_bishop1")
+	    w_bishop1 = game.pieces.find_by_piece_name("w_bishop1")
+	    w_bishop2 = game.pieces.find_by_piece_name("w_bishop2")
+	    b_bishop1 = game.pieces.find_by_piece_name("b_bishop1")
+	    b_bishop2 = game.pieces.find_by_piece_name("b_bishop2")
 
-	    expect(bishop.valid_move?(5, 4)).to eq(true)
+	  it "should allow bishops to move diagonally" do
+	    expect(w_bishop1.valid_move?(5, 4)).to eq(true)
+	    expect(w_bishop2.valid_move?(5, 3)).to eq(true)
+	    expect(b_bishop1.valid_move?(2, 4)).to eq(true)
+	    expect(b_bishop2.valid_move?(2, 3)).to eq(true)
 	  end
 
 	  it "should prevent bishops from moving horizontally" do
-		game = FactoryGirl.create(:game)
-		game.board = [
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,e,e,o,o,o],
-							[o,o,o,o,x,o,o,o],
-							[o,o,x,o,o,x,o,o],
-						]
-	    user = FactoryGirl.create(:user)
-	    bishop = FactoryGirl.create(:bishop, piece_color: "white", x_coordinate: 2, y_coordinate: 7, user_id: user.id, game_id: game.id)
-
-	    expect(bishop.valid_move?(4, 7)).to eq(false)
+	    expect(w_bishop1.valid_move?(4, 7)).to eq(false)
 	  end
 
 	  it "should prevent bishops from moving vertically" do
-		game = FactoryGirl.create(:game)
-		game.board = [
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,e,e,o,o,o],
-							[o,o,o,o,x,o,o,o],
-							[o,o,x,o,o,x,o,o],
-						]
-	    user = FactoryGirl.create(:user)
-	    bishop = FactoryGirl.create(:bishop, piece_color: "white", x_coordinate: 2, y_coordinate: 7, user_id: user.id, game_id: game.id)
-
-	    expect(bishop.valid_move?(2, 3)).to eq(false)
+	    expect(b_bishop2.valid_move?(4, 5)).to eq(false)
 	  end
 
 	  it "should prevent unallowed moves" do
-		game = FactoryGirl.create(:game)
-		game.board = [
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,o,o,o,o,o],
-							[o,o,o,e,e,o,o,o],
-							[o,o,o,o,x,o,o,o],
-							[o,o,x,o,o,x,o,o],
-						]
-	  	user = FactoryGirl.create(:user)
-	  	bishop = FactoryGirl.create(:bishop, piece_color: "white", x_coordinate: 2, y_coordinate: 7, user_id: user.id, game_id: game.id)
-
-	  	expect(bishop.valid_move?(3, 2)).to eq(false)
+			expect(w_bishop2.valid_move?(6, 1)).to eq(false)
 	  end
 	end
 
