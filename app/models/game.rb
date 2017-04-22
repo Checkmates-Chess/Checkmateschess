1e06ac68c48a929f045ead49d746dd41581ac4cb
Evan Cording <evancording@gmail.com>
2017-04-22 23:29:20 +0000

    in check method in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -57,4 +57,104 @@ class Game < ApplicationRecord
                 [@w_rook1, @w_knight1, @w_bishop1, @w_king, @w_queen, @w_bishop2, @w_knight2, @w_rook2],
               ]
   end
+
+  def in_check?
+    side_in_check?("black") || side_in_check?("white")
+  end
+
+  def side_in_check?(color)
+    enemy = color == "white" ? "black" : "white"
+    #white king in check
+    #white_king = nil
+    #white_king_row = nil
+    #white_king_col = nil
+    if game.pieces.exists?(piece_type: "King", piece_color: color)
+      king = game.pieces.where(piece_type: "King", piece_color: color).first
+      king_row = king.y_coordinate
+      king_col = king.x_coordinate
+    end
+
+    game.pieces.where(piece_color: enemy).each do |piece|
+      if piece.piece_type != "Pawn" && !piece.x_coordinate.nil? && !piece.y_coordinate.nil?
+        if piece.valid_move?(king_row, king_col)
+          return true
+        end
+      end
+    end
+    
+    #game.pieces.where(piece_type: "Bishop", piece_color: enemy).each do |bishop|
+    #  if bishop.valid_move?(white_king_row, white_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "Knight", piece_color: enemy).each do |knight|
+    #  if knight.valid_move?(white_king_row, white_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "Queen", piece_color: enemy).each do |queen|
+    #  if queen.valid_move?(white_king_row, white_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "King", piece_color: enemy).each do |king|
+    #  if king.valid_move?(white_king_row, white_king_col)
+    #    return true
+    #  end
+    #end
+    game.pieces.where(piece_type: "Pawn", piece_color: enemy).each do |pawn|
+      if !pawn.x_coordinate.nil? && !pawn.y_coordinate.nil?
+        if pawn.can_capture?(king_row, king_col)
+          return true
+        end
+      end
+      #if pawn.y_coordinate + 1 == white_king_row && (pawn.x_coordinate - 1 == white_king_col || pawn.x_coordinate + 1 == white_king_col)
+      #  return true
+      #end
+    end
+
+    #black king in check
+    #black_king = nil
+    #black_king_row = nil
+    #black_king_col = nil
+    #if game.pieces.exists?(piece_type: "King", piece_color: "black")
+    #  black_king = game.pieces.where(piece_type: "King", piece_color: "black").first
+    #  black_king_row = black_king.y_coordinate
+    #  black_king_col = black_king.x_coordinate
+    #end
+
+    #game.pieces.where(piece_type: "Rook", piece_color: "white").each do |rook|
+    #  if rook.valid_move?(black_king_row, black_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "Bishop", piece_color: "white").each do |bishop|
+    #  if bishop.valid_move?(black_king_row, black_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "Knight", piece_color: "white").each do |knight|
+    #  if knight.valid_move?(black_king_row, black_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "Queen", piece_color: "white").each do |queen|
+    #  if queen.valid_move?(black_king_row, black_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "King", piece_color: "white").each do |king|
+    #  if king.valid_move?(black_king_row, black_king_col)
+    #    return true
+    #  end
+    #end
+    #game.pieces.where(piece_type: "Pawn", piece_color: "white").each do |pawn|
+    #  if pawn.y_coordinate - 1 == black_king_row && (pawn.x_coordinate - 1 == black_king_col || pawn.x_coordinate + 1 == black_king_col)
+    #   return true
+    #  end
+    #end
+
+    false
+  end
+
 end
