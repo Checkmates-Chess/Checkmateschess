1c68f4db6278fcd8c00e2d0d17dbfe04806fd4e2
kbanico <code4uguam@gmail.com>
2017-04-25 08:48:37 +0000

    added test for checkmate function

--- app/models/king.rb
+++ app/models/king.rb
@@ -8,6 +8,7 @@ class King < Piece
 
 
 
+
   def move_out_of_check?
     starting_x = x_coordinate
     starting_y = y_coordinate
@@ -15,11 +16,11 @@ class King < Piece
     # still in check
     success = false
     ((x_coordinate-1)..(x_coordinate+1)).each do |x|
-      ((y_coordinate-1).. (y_coordinate+1)).each do |y|
+      ((y_coordinate-1)..(y_coordinate+1)).each do |y|
 
         # check if that square is available
         self.update_attributes(x_coordinate:x, y_coordinate: y) if valid_move?(x,y)
-        success = true if game.in_check?(piece_color) == false
+        success = true if self.game.side_in_check?(self.piece_color) == false
 
         #reset the king to redo moves
         self.update_attributes(x_coordinate: starting_x, y_coordinate: starting_y)
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,23 +1,58 @@
 require 'rails_helper'
-describe King do
-  let(:game) { FactoryGirl.create(:game)}
-  let(:pawn) do
-    FactoryGirl.create(:piece, piece_type: "Pawn", game:game, user:game.user,
-      x_coordinate: 1, y_coordinate: 2, piece_color: "white")
+RSpec.describe King do
+  # let(:game) { FactoryGirl.create(:game)}
+  # let(:pawn) do
+  #   FactoryGirl.create(:piece, piece_type: "Pawn", game:game, user:game.user,
+  #     x_coordinate: 1, y_coordinate: 2, piece_color: "white")
 
+  # end
+
+  # let(:king) do
+  #     FactoryGirl.create(:piece, piece_type: "King", game:game, user:game.user,
+  #     x_coordinate: 2, y_coordinate: 2, piece_color: "black")
+  # end
+
+  # let(:knight) do
+  #     FactoryGirl.create(:piece, piece_type: "Knight", game:game, user:game.user,
+  #     x_coordinate: 1, y_coordinate: 0, piece_color: "white")
+
+  # end
+
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @game = FactoryGirl.create(:game)
+    @knight = Piece.create(piece_type: "Knight", x_coordinate: 1,y_coordinate: 0, piece_color: "white", game: @game, user: @user)
+    @king = Piece.create(piece_type: "King", x_coordinate: 2,y_coordinate: 2, piece_color: "black", game: @game, user:@user)
   end
 
-  let(:king) do
-      FactoryGirl.create(:piece, piece_type: "King", game:game, user:game.user,
-      x_coordinate: 2, y_coordinate: 2, piece_color: "black")
-
+  after(:all) do
+    User.destroy_all
   end
 
-  describe "#checkmate?" do
-    context "current game should not be checkmate" do
-      it {expect(game.checkmate?(king.piece_color)).to eq(false)}
+  describe "#move_out_of_check?" do
+    context "king should be able to move out of check" do
+      it{
+        expect(@king.move_out_of_check?).to eq(true)
+      }
     end
+
+    context "#side_in_check with a piece checking king" do
+      it{
+        expect(@game.side_in_check?(@king.piece_color)).to eq(true)
+      }
+    end
+
+    context "#side_in_check without a piece checking king" do
+      it{
+        @knight.update_attributes(x_coordinate:0, y_coordinate:0)
+        @knight.reload
+        expect(@game.side_in_check?(@king.piece_color)).to eq(false)
+      }
+    end
+
+    context "#checkmate? with a king that can run away" do
+      it{expect(@game.checkmate?(@king.piece_color)).to eq(false)}
+    end
+
   end
-
-
 end
