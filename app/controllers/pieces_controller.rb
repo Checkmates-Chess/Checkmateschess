30bc3664d96d9ac2cbfd548f99fbb2ba5c8432c3
Evan Cording <evancording@gmail.com>
2017-04-25 01:09:57 +0000

    some changes to pieces controller and pieces controller spec

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -6,9 +6,28 @@ class PiecesController < ApplicationController
 		@piece = @game.pieces.create(piece_params)
 	end
 
-	def update
+	def show
     @piece = Piece.find(params[:id])
+    current_piece_status = @piece.piece_status
+    if current_piece_status.nil?
+      @piece.update_attributes(piece_status: "highlighted")
+    else
+      @piece.update_attributes(piece_status: current_piece_status + "|highlighted")
+    end
+    redirect_to game_path(@piece.game)
+  end
+
+  def update
+    @piece = Piece.find(params[:id])
+    #@game = @piece.game.find(@piece.game_id)
+    #@game = Game.find(params[:piece][:game_id])    
+    @game = @piece.game
+    @game.board[@piece.y_coordinate][@piece.x_coordinate] = nil
     @piece.update_attributes(piece_params)
+    @game.board[@piece.y_coordinate][@piece.x_coordinate] = @piece
+    new_status = @piece.piece_status
+    new_status.sub! "|highlighted", ""
+    @piece.update_attributes(piece_status: new_status)
     redirect_to game_path(@piece.game)
   end
 
