50848678d327e49bab0c5cae2451521362dd0399
Evan Cording <evancording@gmail.com>
2017-04-29 18:44:44 +0000

    js change kind of working

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,7 +4,7 @@ module GamesHelper
     game = @game
     piece = game.pieces.find_by_y_coordinate_and_x_coordinate(col, row)
     if piece
-      image_tag("#{piece.piece_name}.svg", class: "pieces", data: {x: "#{row}", y: "#{col}", pid: "#{piece.id}"}) 
+      image_tag("#{piece.piece_name}.svg", class: "pieces", data: {pid: "#{piece.id}"}) 
     end
   end
 end
