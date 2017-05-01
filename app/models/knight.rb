2ab1e55ba6a9917bdb54bdce6cf91ed42a8e2e34
Evan Cording <evancording@gmail.com>
2017-05-01 02:35:29 +0000

    adjusted knight valid move a little using move_to branch veresion

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,9 +1,13 @@
 class Knight < Piece
   def valid_move?(y, x)
-     # check valid move from piece.rb and check if x and y of knight is valid
-     diff_x = x - x_coordinate
-     diff_x_abs = diff_x.abs
-     diff_y = (y - y_coordinate).abs
-     super && ((diff_x == 2 && diff_y == 1) || (diff_x == 1 && diff_y == 2))
+    # check valid move from piece.rb and check if x and y of knight is valid
+    diff_x = x - x_coordinate
+    diff_x_abs = diff_x.abs
+    diff_y = y - y_coordinate
+    if piece_color = "black"
+      return super && ((diff_x == 2 && diff_y == 1) || (diff_x == 1 && diff_y == 2))
+    else 
+      return super && ((diff_x == 2 && diff_y == -1) || (diff_x == 1 && diff_y == -2))
+    end
   end
 end
