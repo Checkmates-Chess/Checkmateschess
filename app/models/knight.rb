8903a48f213d0c8d2787c09d0e07e56caf5806df
evan10003 <evancording@gmail.com>
2017-05-03 18:52:56 -0400

    Update knight.rb
    changing knight to allow all movement, matches turn_etc to be merged...for some reason conflict not showing up

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -4,14 +4,9 @@ class Knight < Piece
     diff_x = x.to_i - x_coordinate.to_i
     diff_x_abs = diff_x.abs
     diff_y = y.to_i - y_coordinate.to_i
-    if piece_color == "black"
-      if (diff_x_abs == 2 && diff_y == 1) || (diff_x_abs == 1 && diff_y == 2)
-        return !friendly_on_endpoint?(y, x)
-      end
-    else 
-      if (diff_x_abs == 2 && diff_y == -1) || (diff_x_abs == 1 && diff_y == -2)
-        return !friendly_on_endpoint?(y, x)
-      end
+    diff_y_abs = diff_y.abs
+    if (diff_x_abs == 2 && diff_y_abs == 1) || (diff_x_abs == 1 && diff_y_abs == 2)
+        return super
     end
     false
   end
