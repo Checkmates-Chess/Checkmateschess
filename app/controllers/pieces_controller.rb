354fff1d9acb2e31522e9bcdff29ab9d7fc3e664
Evan Cording <evancording@gmail.com>
2017-05-04 06:38:01 +0000

    adding two stage process for pawn promotion through pieces#update...incomplete

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -18,10 +18,14 @@ class PiecesController < ApplicationController
     remove_flag = false
     in_check = false
     pawn_promotion = false
+    promote_to = nil
     piece_type = @piece.piece_type
 
+    # check for pawn promotion  
+    if @piece.piece_status.include?("promote to")
+
     # checking for allowed move and updating piece
-    if valid_move
+    elsif valid_move
       @piece.update_attributes(x_coordinate: new_x, y_coordinate: new_y)
       in_check = @game.side_in_check?(color)
       @piece.update_attributes(x_coordinate: old_x, y_coordinate: old_y)
@@ -33,6 +37,22 @@ class PiecesController < ApplicationController
 
     pawn_promotion = @piece.pawn_promotion?(new_y, new_x)
 
+    rook_el = nil
+    queen_el = nil
+    bishop_el = nil
+    knight_el = nil
+    if color == "black"
+      rook_el = image_tag("b_rook1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+      queen_el = image_tag("b_queen.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+      bishop_el = image_tag("b_bishop1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+      knight_el = image_tag("b_knight1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    else
+      rook_el = image_tag("w_rook1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+      queen_el = image_tag("w_queen.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+      bishop_el = image_tag("w_bishop1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+      knight_el = image_tag("w_knight1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    end
+
     json_piece = {
       x_coordinate: @piece.x_coordinate,
       y_coordinate: @piece.y_coordinate,
@@ -41,7 +61,8 @@ class PiecesController < ApplicationController
       old_x: old_x,
       old_y: old_y,
       color: color,
-      piece_type: piece_type
+      piece_type: piece_type,
+      promote_to: promote_to
     }
     render json: json_piece
   end
