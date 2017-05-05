1c0e252c12f96226f7febc7b83da300b1ef53f9a
Evan Cording <evancording@gmail.com>
2017-05-05 02:22:27 +0000

    test to change pawn image to rook image is working, will implement fully later

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -18,12 +18,10 @@ class PiecesController < ApplicationController
     remove_flag = false
     in_check = false
     pawn_promotion = false
-    promote_to = nil
-    piece_type = @piece.piece_type
 
     # check for pawn promotion  
     if @piece.piece_status.include?("promote to")
-
+      @piece.update_attributes(piece_type: "Rook", piece_status: "alive")
     # checking for allowed move and updating piece
     elsif valid_move
       @piece.update_attributes(x_coordinate: new_x, y_coordinate: new_y)
@@ -31,27 +29,26 @@ class PiecesController < ApplicationController
       @piece.update_attributes(x_coordinate: old_x, y_coordinate: old_y)
       if !in_check
         remove_flag = @piece.move_to!(new_y, new_x)
+        pawn_promotion = @piece.pawn_promotion?(new_y, new_x)
         @game.switch_turn
       end
     end
 
-    pawn_promotion = @piece.pawn_promotion?(new_y, new_x)
-
-    rook_el = nil
-    queen_el = nil
-    bishop_el = nil
-    knight_el = nil
-    if color == "black"
-      rook_el = image_tag("b_rook1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-      queen_el = image_tag("b_queen.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-      bishop_el = image_tag("b_bishop1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-      knight_el = image_tag("b_knight1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-    else
-      rook_el = image_tag("w_rook1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-      queen_el = image_tag("w_queen.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-      bishop_el = image_tag("w_bishop1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-      knight_el = image_tag("w_knight1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
-    end
+    #rook_el = nil
+    #queen_el = nil
+    #bishop_el = nil
+    #knight_el = nil
+    #if color == "black"
+    #  rook_el = image_tag("b_rook1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    #  queen_el = image_tag("b_queen.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    #  bishop_el = image_tag("b_bishop1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    # knight_el = image_tag("b_knight1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    #else
+    # rook_el = image_tag("w_rook1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    #  queen_el = image_tag("w_queen.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    #  bishop_el = image_tag("w_bishop1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    #  knight_el = image_tag("w_knight1.svg", class: "pieces", data: {pid: "#{@piece.id}"})
+    #end
 
     json_piece = {
       x_coordinate: @piece.x_coordinate,
@@ -61,8 +58,9 @@ class PiecesController < ApplicationController
       old_x: old_x,
       old_y: old_y,
       color: color,
-      piece_type: piece_type,
-      promote_to: promote_to
+      piece_type: @piece.piece_type,
+      piece_name: @piece.piece_name
+      #image_tag: image_tag
     }
     render json: json_piece
   end
